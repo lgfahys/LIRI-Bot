33b458c0c1d36080bb42a4326b384b813c591e2e
lgfahys <lindsey@codeasite.com>
2019-04-14 23:33:08 -0400

    added do-what-it-says case

--- liri.js
+++ liri.js
@@ -22,9 +22,15 @@ var search = process.argv.slice(3, process.argv.length).join(" ");
 
 // setting up spotify search function
 
-function spotifySearch() {
+function spotifySearch(search) {
+  var song;
 
-  spotify.search({ type: 'track', query: search }, function (err, data) {
+  if (search === null || search === undefined || search === "") {
+    song = "I Want It That Way";
+  } else {
+    song = search;
+  }
+  spotify.search({ type: 'track', query: song }, function (err, data) {
     if (err) {
       return console.log('Error occurred: ' + err);
     }
@@ -33,11 +39,11 @@ function spotifySearch() {
     var spotifySearchResult = data.tracks.items[0];
     // console.log(spotifySearchResult);
     for (i = 0; i < spotifySearchResult.artists.length; i++) {
-      console.log("----------Song Info----------")
-      console.log("Artist(s): " + spotifySearchResult.artists[i].name);
-      console.log("Song Name: " + spotifySearchResult.name);
-      console.log("Preview Link: " + spotifySearchResult.preview_url);
-      console.log("Album: " + spotifySearchResult.album.name);
+      console.log("\n----------Song Info----------\n")
+      console.log("\033[38;5;6m" + "Artist(s): " + "\033[0m" + spotifySearchResult.artists[i].name);
+      console.log("\033[38;5;6m" + "Song Name: " + "\033[0m" + spotifySearchResult.name);
+      console.log("\033[38;5;6m" + "Preview Link: " + "\033[0m" + spotifySearchResult.preview_url);
+      console.log("\033[38;5;6m" + "Album: " + "\033[0m" + spotifySearchResult.album.name + "\n");
       console.log("---------------------------------");
     }
   });
@@ -46,12 +52,21 @@ function spotifySearch() {
 
 // setting up bandsintown search
 
-function bandsSearch() {
+function bandsSearch(search) {
+  var artist;
 
-  var artist = search;
+  if (search === null || search === undefined || search === "") {
+    artist = "Post Malone";
+  } else {
+    artist = search;
+  }
 
   axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(
-    function (response) {
+    function (response, error) {
+
+      if (error) {
+        return console.log(error);
+      }
       console.log("\n Here are upcoming event details for " + artist + "...\n")
       for (i = 0; i < response.data.length; i++) {
         console.log("----------Event Info----------" + "\n");
@@ -64,10 +79,20 @@ function bandsSearch() {
   );
 }
 
-function omdbSearch() {
-  var movie = search;
+function omdbSearch(search) {
+  var movie;
+
+  if (search === null || search === undefined || search === "") {
+    movie = "Mr. Nobody";
+  } else {
+    movie = search;
+  }
   axios.get("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy").then(
-    function (response) {
+    function (response, error) {
+
+      if (error) {
+        return console.log(error);
+      }
       console.log("\n Here are the details for " + movie + "...\n");
       console.log("----------Movie Info----------" + "\n");
       console.log("\033[38;5;6m" + "Title: " + "\033[0m" + response.data.Title);
@@ -79,65 +104,85 @@ function omdbSearch() {
       console.log("\033[38;5;6m" + "Plot: " + "\033[0m" + response.data.Plo);
       console.log("\033[38;5;6m" + "Actors: " + "\033[0m" + response.data.Actors + "\n");
       console.log("------------------------------");
-      // * Title of the movie.
-      // * Year the movie came out.
-      // * IMDB Rating of the movie.
-      // * Rotten Tomatoes Rating of the movie.
-      // * Country where the movie was produced.
-      // * Language of the movie.
-      // * Plot of the movie.
-      // * Actors in the movie.
     }
   );
 }
 
+// creating a function to display message if nothing is entered
 function nullCase() {
   console.log("\n" + "\033[38;5;9m" + "Command not found. Type 'node liri.js help' for more information");
 }
 
+// creating a function to display instructions for user to see
 function help() {
   console.log(
-  "\n" +
-  "STRUCTURE" +
-  "\033[38;5;6m" + "\n  node liri.js (command) + (search(artist, concert, movie))\n" + "\033[0m" + 
-  "\nCOMMAND OPTIONS" + 
-  "\033[38;5;10m" +
-  "\n  spotify-this-song" + "\033[0m" + "     search for artist details" +
-  "\033[38;5;10m" +
-  "\n  concert-this" + "\033[0m" + "          search for concert details" +
-  "\033[38;5;10m" +
-  "\n  movie-this" + "\033[0m" + "            search for movie details"
+    "\n" +
+    "STRUCTURE" +
+    "\033[38;5;6m" + "\n  node liri.js (command) + (search(artist, concert, movie))\n" + "\033[0m" +
+    "\nCOMMAND OPTIONS" +
+    "\033[38;5;10m" +
+    "\n  spotify-this-song" + "\033[0m" + "     search for artist details" +
+    "\033[38;5;10m" +
+    "\n  concert-this" + "\033[0m" + "          search for concert details" +
+    "\033[38;5;10m" +
+    "\n  movie-this" + "\033[0m" + "            search for movie details"
   )
 }
 
 // appending the argument and search to random.txt
-fs.appendFile("random.txt", "\n" + argument + ", " + search, function (err) {
+fs.appendFile("random.txt", "\n" + argument + "," + search + ",", function (err) {
   if (err) {
     console.log(err);
   }
 
 });
 
+// reading what is in random.txt and searching
+function readFile() {
+  fs.readFile("random.txt", "utf8", function (error, data) {
+
+    if (error) {
+      return console.log(error);
+    }
+
+    var data = data.split(",");
+    argument = data[0];
+    search = data[1];
+    console.log(argument, search);
+
+    runSearch(argument, search);
+
+  });
+}
+
+runSearch(argument, search);
+
 // switch case to detect which argument was entered
-switch (argument) {
-  case "spotify-this-song":
-    spotifySearch();
-    break;
+function runSearch(argument, search) {
+  switch (argument) {
+    case "spotify-this-song":
+      spotifySearch(search);
+      break;
 
-  case "movie-this":
-    omdbSearch();
-    break;
+    case "movie-this":
+      omdbSearch(search);
+      break;
 
-  case "concert-this":
-    bandsSearch();
-    break;
+    case "concert-this":
+      bandsSearch(search);
+      break;
 
-  case "help":
-    help();
-    break;
+    case "do-what-it-says":
+      readFile(search);
+      break;
 
-  default:
-    nullCase();
+    case "help":
+      help();
+      break;
+
+    default:
+      nullCase();
+  }
 }
 
 
